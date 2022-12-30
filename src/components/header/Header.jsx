fd8004a231fffea54b98df4fc7c64a1fb6bca469
Andy Yang <yangandy1001@gmail.com>
2022-12-30 12:18:01 -0500

    finished header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import css from './Header.module.css'
-
+import Logo from '../../assets/logo.png'
+import {CgShoppingBag} from 'react-icons/cg'
 const Header = () => {
   return (
     <div className={css.container}>
         <div className={css.logo}>
-            logo
+            <img src={Logo} alt="logo" />
+            <span>AY Commmerce</span>
         </div>
 
         <div className={css.right}>
             <div className={css.menu}>
-
+                <ul className={css.menu}>
+                    <li>Collections</li>
+                    <li>Brands</li>
+                    <li>New</li>
+                    <li>Sales</li>
+                    <li>ENG</li>
+                </ul>
             </div>
-            <input type="text" className={css.search}/>
-            <span>PNG icon of cart</span>
+            <input type="text" className={css.search} placeholder='search'/>
+            <CgShoppingBag className={css.cart}/>
         </div>
     </div>
   )
