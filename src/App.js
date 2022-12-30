73af0181a96fc8697d6ef40f8bb9f472d7b2684b
Andy Yang <yangandy1001@gmail.com>
2022-12-30 12:03:06 -0500

    yarn, added necessary styles, onto first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 
-function App() {
+/*
+    This is our application's top-level component.
+    
+    @author Andy Yang
+*/
+/*
+  This is the entry-point for our application. Many different home pages
+  
+  @author Andy Yang
+*/
+
+import React from 'react'
+import Header from "./components/header/Header";
+
+const App = () => {
   return (
-    <div className="App">
-      <span>starting APp</span>
+    <div className="app">
+      <Header/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
