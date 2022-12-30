96b05ac4c883889fd6961f78f5f08c77dc0fa659
Andy Yang <yangandy1001@gmail.com>
2022-12-30 16:48:29 -0500

    working on hero section

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ import "./App.css";
 */
 
 import React from 'react'
-import Header from "./components/header/Header";
+import { Header,Hero } from "./components";
 
 const App = () => {
   return (
     <div className="app">
       <Header/>
+      <Hero/>
     </div>
   )
 }
