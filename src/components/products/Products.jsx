c781b751ef47cc9fd6f638267f1bd20b621edcd7
Andy Yang <yangandy1001@gmail.com>
2023-01-02 12:23:31 -0500

    almost done with products grid

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,11 +4,13 @@ import Plane from "../../assets/plane.png";
 import { ProductsData } from "../../data/products";
 import { useState } from "react";
 const Products = () => {
-    const [menuProducts, setMenuProducts] = useState(ProductsData);
+  const [menuProducts, setMenuProducts] = useState(ProductsData);
   return (
     <div className={css.container}>
-      <img src={Plane} alt="" />
-      <h1>Our Featured Products</h1>
+      <div className={css.header}>
+        <img src={Plane} alt="" />
+        <h1>Our Featured Products</h1>
+      </div>
 
       <div className={css.products}>
         <ul className={css.menu}>
@@ -19,13 +21,19 @@ const Products = () => {
         </ul>
 
         <div className={css.list}>
-            {
-                menuProducts.map((product, i)=>(
-                    <div className={css.product}>
-                        Products
+          {menuProducts.map((product, i) => (
+            <div className={css.product}>
+                <div className="left-s">
+                    <div className="name">
+                        <span>{product.name}</span>
+                        <span>{product.detail}</span>
                     </div>
-                ))
-            }
+                    <span>{product.price}</span>
+                    <div>Shop Now</div>
+                </div>
+                <img src={product.img} alt="" className="img-p"/>
+            </div>
+          ))}
         </div>
       </div>
     </div>
