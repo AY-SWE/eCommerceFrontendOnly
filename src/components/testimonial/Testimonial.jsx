050b629ba83b989af08b1d32be1f884f5f7e2f47
Andy Yang <yangandy1001@gmail.com>
2023-01-03 15:25:01 -0500

    almost done with testimonial

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Hero from "../../assets/testimonialHero.png";
 import css from "./Testimonial.module.css";
+import { TestimonialsData } from "../../data/testimonials";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 const Testimonial = () => {
   return (
     <div className={css.testimonials}>
@@ -19,6 +22,26 @@ const Testimonial = () => {
           <span>100k</span>
           <span>Happy Customer Review</span>
         </div>
+      </div>
+      <div className={css.reviews}>Reviews</div>
+      <div className={css.carousel}>
+        <Swiper
+          className={css.tCarousel}
+          slidesPerView={3}
+          spaceBetween={25}
+          slidesPerGroup={1}
+        >
+          {TestimonialsData.map((testimonial, i) => (
+            <SwiperSlide>
+              <div className={css.testimonial}>
+                <img src={testimonial.image} alt="" />
+                <span>{testimonial.comment}</span>
+                <hr />
+                <span>{testimonial.name}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
