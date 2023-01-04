1fb84e665a9d49134e1045c88f642e25ba10b410
Andy Yang <yangandy1001@gmail.com>
2023-01-04 15:16:10 -0500

    finished most of footer, need to add copyright item

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,75 @@
-import React from 'react'
-import css from './Footer.module.css'
-import Logo from '../../assets/logo.png'
-import {InboxIcon, PhoneIcon,LocationMarkerIcon,LoginIcon,UsersIcon,LinkIcon} from '@heroicons/react/outline'
+import React from "react";
+import css from "./Footer.module.css";
+import Logo from "../../assets/logo.png";
+import {
+  InboxIcon,
+  PhoneIcon,
+  LocationMarkerIcon,
+  LoginIcon,
+  UsersIcon,
+  LinkIcon,
+} from "@heroicons/react/outline";
 
 const Footer = () => {
   return (
     <div className={css.cFooterWrapper}>
-        <hr />
-        <div className={css.cFooter}>
-            <div className={css.logo}>
-                <img src={Logo} alt="" />
-                <span>AY Commerce</span>
-            </div>
+      <hr />
+      <div className={css.cFooter}>
+        <div className={css.logo}>
+          <img src={Logo} alt="" />
+          <span>AY Commerce</span>
         </div>
-    </div>
-  )
-}
 
-export default Footer
+        <div className={css.block}>
+          <div className={css.details}>
+            <span>Contact Us</span>
+            <span className={css.pngLine}>
+              <LocationMarkerIcon className={css.icon} />
+              <span>8 Avenue, New York City, NY 88888</span>
+            </span>
+            <span className={css.pngLine}>
+              <PhoneIcon className={css.icon} />
+              <span>(888)888-8888</span>
+            </span>
+            <span className={css.pngLine}>
+              <InboxIcon className={css.icon} />
+              <span>support@aycommerce.com</span>
+            </span>
+          </div>
+        </div>
+
+        <div className={css.block}>
+          <div className={css.details}>
+            <span>Account</span>
+            <span className={css.pngLine}>
+              <LoginIcon className={css.icon} />
+              <span>Sign in</span>
+            </span>
+          </div>
+        </div>
+
+        <div className={css.block}>
+          <div className={css.details}>
+            <span>Company</span>
+            <span className={css.pngLine}>
+              <UsersIcon className={css.icon} />
+              <span>About Us</span>
+            </span>
+          </div>
+        </div>
+
+        <div className={css.block}>
+          <div className={css.details}>
+            <span>Resources</span>
+            <span className={css.pngLine}>
+              <LinkIcon className={css.icon} />
+              <span>Safety & Privacy</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
